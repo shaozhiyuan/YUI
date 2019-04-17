059281ff7a92be1b31ed42838a1f4be9b0052fb0
邵志远 <shaozhiyuan@huohua.cn>
2019-04-17 21:19:33 +0800

    npm包变成小写

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
-        library: 'YUI',
+        library: 'yuan-ui',
         libraryTarget: 'umd',
     },
     resolve: {
