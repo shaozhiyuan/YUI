d800dc574f814c98e40eec743606f3d77a9277b3
邵志远 <shaozhiyuan@huohua.cn>
2019-04-18 20:36:46 +0800

    onclick类型完善

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -3,12 +3,13 @@ import './importIcons';
 import './icon.scss';
 
 interface IconProps {
-    name: string,
+    name: string;
+    onClick: React.MouseEventHandler<SVGElement>
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <svg className="yui-icon">
+        <svg className="yui-icon" onClick={props.onClick}>
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     );
--- lib/index.tsx
+++ lib/index.tsx
@@ -3,6 +3,10 @@ import ReactDom from 'react-dom';
 import Icon from './icon';
 
 
+const fn: React.MouseEventHandler = (e) => {
+  console.log(e.target)
+};
+
 ReactDom.render(<div>
-    <Icon name='wechat'/>
+    <Icon name='wechat' onClick={fn}/>
 </div>, document.getElementById('root'));
