24ce9d20cc284b6bd3c35ecdceced8eac353c88e
shaozhiyuan <632068012@qq.com>
2019-08-15 19:32:15 +0800

    打包发布配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,11 +1,1 @@
-import * as React from 'react';
-import ReactDom from 'react-dom';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler<SVGAElement | SVGUseElement> = (e) => {
-    console.log((e.target as HTMLDivElement));
-};
-
-ReactDom.render(<div>
-    <Icon name="wechat" onClick={fn}/>
-</div>, document.getElementById('root'));
+export {default as Icon} from './icon/icon';
