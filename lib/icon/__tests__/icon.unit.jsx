8e8553c46cdd593903e32902dd93c75b34eb392d
shaozhiyuan <632068012@qq.com>
2019-08-15 19:11:20 +0800

    delete

--- lib/icon/__tests__/icon.unit.jsx
+++ lib/icon/__tests__/icon.unit.jsx
@@ -1,17 +1,8 @@
-import * as renderer from 'react-test-renderer'
 import Icon from '../icon';
 import React from 'react';
 import {mount} from 'enzyme';
 
 describe('Icon', () => {
-    it('他是个icon,svg', () => {
-        const json = renderer.create(<Icon name="wechat"/>).toJSON();
-        expect(json).toMatchSnapshot()
-    });
-    it('render successfully', () => {
-        const json = renderer.create(<Icon name="wechat"/>).toJSON();
-        expect(json).toMatchSnapshot()
-    });
     it('onClick', () => {
         const fn = jest.fn();
         const c = mount(<Icon name='wechat' onClick={fn}/>);
