bef59186c4d6866af657addc933c15a238a61bdf
邵志远 <shaozhiyuan@huohua.cn>
2019-09-03 17:22:13 +0800

    dialog组件的三个api完成

--- example.tsx
+++ example.tsx
@@ -5,31 +5,36 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import Icon from './lib/icon/icon';
 import IconExample from './lib/icon/icon.example';
 import ButtonExample from './lib/button.example';
+import DialogExample from './lib/dialog/dialog.example';
 
 ReactDom.render((
-    <Router>
-        <div>
-            <header>
-                <div className="logo">YUI</div>
-            </header>
-            <Icon name="left"/>
-        </div>
-        <div>
-            <aside>
-                <h2>组件</h2>
-                <ul>
-                    <li>
-                        <Link to="/icon">Icon</Link>
-                    </li>
-                    <li>
-                        <Link to="/button">Button</Link>
-                    </li>
-                </ul>
-            </aside>
-            <main>
-                <Route path="/icon" component={IconExample}/>
-                <Route path="/button" component={ButtonExample}/>
-            </main>
-        </div>
-    </Router>
+  <Router>
+    <div>
+      <header>
+        <div className="logo">YUI</div>
+      </header>
+      <Icon name="left"/>
+    </div>
+    <div>
+      <aside>
+        <h2>组件</h2>
+        <ul>
+          <li>
+            <Link to="/icon">Icon</Link>
+          </li>
+          <li>
+            <Link to="/button">Button</Link>
+          </li>
+          <li>
+            <Link to="/dialog">对话框</Link>
+          </li>
+        </ul>
+      </aside>
+      <main>
+        <Route path="/icon" component={IconExample}/>
+        <Route path="/button" component={ButtonExample}/>
+        <Route path="/dialog" component={DialogExample}/>
+      </main>
+    </div>
+  </Router>
 ), document.querySelector('#root'));
---
+++ lib/dialog/dialog.tsx
@@ -0,0 +1,92 @@
+import React, {Fragment, ReactElement, ReactFragment, ReactNode} from 'react';
+import ReactDOM from 'react-dom';
+import './dialog.scss';
+import {scopedClassMaker} from '../classes';
+import Icon from '../icon/icon';
+
+interface Props {
+  visible: boolean,
+  buttons?: ReactFragment | ReactElement,
+  onClose: React.MouseEventHandler,
+  closeOnClickMask?: boolean
+}
+
+const sc = scopedClassMaker('yui-dialog');
+
+const Dialog: React.FunctionComponent<Props> = (props) => {
+  const onClickClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  };
+  const onClickMask: React.MouseEventHandler = (e) => {
+    if (props.closeOnClickMask) {
+      props.onClose(e);
+    }
+  };
+  const x = props.visible ?
+    <Fragment>
+      <div className={sc('mask')} onClick={onClickMask}></div>
+      <div className={sc()}>
+        <div className={sc('close')} onClick={onClickClose}>
+          <Icon name="close"/>
+        </div>
+        <header className={sc('header')}>提示</header>
+        <main className={sc('main')}>
+          {props.children}
+        </main>
+        <footer className={sc('footer')}>
+          {props.buttons}
+        </footer>
+      </div>
+    </Fragment> :
+    null;
+  return (
+    ReactDOM.createPortal(x, document.body)
+  );
+};
+
+
+Dialog.defaultProps = {
+  closeOnClickMask: false
+};
+
+
+const Modal = (content: ReactNode, buttons?: ReactFragment | ReactElement, afterClose?: () => void) => {
+  const close = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+    afterClose && afterClose();
+    return true;
+  };
+  const component =
+    <Dialog
+      visible={true}
+      onClose={close}
+      buttons={buttons}
+    >{content}</Dialog>;
+  const div = document.createElement('div');
+  document.body.append(div);
+  ReactDOM.render(component, div);
+  return close;
+};
+
+export const alert = (content: ReactNode, callback?: () => void) => {
+  const close = Modal(content,
+    <button onClick={() => close() && callback && callback()}>确定</button>);
+};
+
+export const confirm = (content: ReactNode, yes: () => void, no?: () => void) => {
+  const close = Modal(content,
+    <Fragment>
+      <button onClick={() => close() && no && no()}>取消</button>
+      <button onClick={() => close() && yes && yes()}>确定</button>
+    </Fragment>
+  );
+};
+
+export const modal = (content: ReactNode) => {
+  return Modal(content);
+};
+
+
+export default Dialog;
