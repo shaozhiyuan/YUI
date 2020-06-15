1de2cc4ee18e83cc48bcbfa3def11fd4643a569f
邵志远 <shaozhiyuan@huohua.cn>
2020-06-15 21:55:35 +0800

    隐藏显示代码按钮

--- demo.tsx
+++ demo.tsx
@@ -8,13 +8,10 @@ interface Props {
 
 const Demo: React.FunctionComponent<Props> = (props) => {
   const [codeVisible, setCodeVisible]= useState(false);
-  return (
-    <div>
-      {props.children}
-      <div>
-        <Highlight {...defaultProps} code={props.code} language="jsx">
-          {({className, style, tokens, getLineProps, getTokenProps}) => (
-            <pre className={className} style={style}>
+  const code = (
+    <Highlight {...defaultProps} code={props.code} language="jsx">
+      {({className, style, tokens, getLineProps, getTokenProps}) => (
+        <pre className={className} style={style}>
         {tokens.map((line, i) => (
           <div {...getLineProps({line, key: i})}>
             {line.map((token, key) => (
@@ -23,8 +20,17 @@ const Demo: React.FunctionComponent<Props> = (props) => {
           </div>
         ))}
       </pre>
-          )}
-        </Highlight>
+      )}
+    </Highlight>
+  );
+  return (
+    <div>
+      {props.children}
+      <div>
+        <button style={{marginBottom: 8}} onClick={() => setCodeVisible(!codeVisible)}>
+          {codeVisible?'隐藏代码':'显示代码'}
+        </button>
+        {codeVisible && code}
       </div>
     </div>
   );
